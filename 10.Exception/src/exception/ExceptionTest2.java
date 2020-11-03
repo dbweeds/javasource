cdab33edbc41fa21e5d81551e74ef4055f4121a8
kimjiho <cafe3ye@naver.com>
2020-11-03 18:15:20 +0900

    오우쮓

--- 10.Exception/src/exception/ExceptionTest2.java
+++ 10.Exception/src/exception/ExceptionTest2.java
@@ -13,6 +13,9 @@ public class ExceptionTest2 {
 		System.out.println(value1);
 		System.out.println(value2);
 		System.out.println("ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ");
+		System.out.println("오우쮓");
+		
+		System.out.println("");
 		
 	}
 
